7cef005382961d487bbff5d3d7cb4c557ec6e496
reja <reja@this.id>
2020-11-30 11:26:15 +0700

    Initial commit

---
+++ app/src/main/java/id/thork/app/pages/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package id.thork.app.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.thork.app.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
