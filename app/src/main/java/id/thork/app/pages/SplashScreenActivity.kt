69f68a187ab75662506da75ad94f89172fe502cf
reja <reja@this.id>
2020-11-30 15:16:54 +0700

    [feature] - add base application and dagger lib

--- app/src/main/java/id/thork/app/pages/SplashScreenActivity.kt
+++ app/src/main/java/id/thork/app/pages/SplashScreenActivity.kt
@@ -3,13 +3,11 @@ package id.thork.app.pages
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.thork.app.R
-import id.thork.app.base.BaseParam
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
 
     }
 }
