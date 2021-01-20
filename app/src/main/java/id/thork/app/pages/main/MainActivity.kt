bfea261180a96ff9e28dbd92a757bc032243763d
this-dev <reza@this.id>
2021-01-20 10:34:16 +0700

    [feature] maps fragment show current location

--- app/src/main/java/id/thork/app/pages/main/MainActivity.kt
+++ app/src/main/java/id/thork/app/pages/main/MainActivity.kt
@@ -12,6 +12,7 @@
 
 package id.thork.app.pages.main
 
+import android.Manifest
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -19,8 +20,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.material.appbar.MaterialToolbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import dagger.hilt.android.AndroidEntryPoint
 import id.thork.app.R
 import id.thork.app.base.BaseActivity
@@ -50,6 +55,7 @@ class MainActivity : BaseActivity(),  View.OnClickListener {
         binding.apply {
             lifecycleOwner = this@MainActivity
         }
+        requestGrantPermissions()
     }
 
     private fun setupBottomNavigationBar() {
@@ -58,14 +64,16 @@ class MainActivity : BaseActivity(),  View.OnClickListener {
         val navGraphIds = listOf(R.navigation.nav_graph_wo, R.navigation.nav_graph_map)
         Timber.tag(TAG).i("setupBottomNavigationBar() navGraphIds: %s", navGraphIds.toString())
 
-        val navController = bottomNavigationView.setupWithNavController(navGraphIds = navGraphIds,
-        fragmentManager =  supportFragmentManager,
-        containerId =  R.id.nav_host_fragment,
-        intent = intent)
+        val navController = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
 
         navController.observe(this, Observer { navController ->
             setupActionBarWithNavController(navController)
-         })
+        })
         currentNavController = navController
     }
 
@@ -80,5 +88,49 @@ class MainActivity : BaseActivity(),  View.OnClickListener {
                 viewModel.checkRepo()
             }
         }
+    }
+
+    /**
+     * Request to Grant Permissions
+     */
+    private fun requestGrantPermissions() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    // check if all permissions are granted
+                    if (report.areAllPermissionsGranted()) {
+                        Timber.tag(TAG).i(
+                            "requestGrantPermissions() report: %s",
+                            report.grantedPermissionResponses.toString()
+                        )
+                        //TODO init location
+//                        validateLocationPermissions()
+//                        onMapReadyState()
+                    }
+
+                    // check for permanent denial of any permission
+                    if (report.isAnyPermissionPermanentlyDenied) {
+                        Timber.tag(TAG).d("requestGrantPermissions() Denied")
+                        //TODO add event when permission permanet Denied
+//                        mapsPresenter.showSettingsDialog()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest>,
+                    token: PermissionToken
+                ) {
+                    token.continuePermissionRequest()
+                }
+            })
+            .onSameThread()
+            .check()
     }
 }
--- app/src/main/java/id/thork/app/pages/main/element/MapFragment.kt
+++ app/src/main/java/id/thork/app/pages/main/element/MapFragment.kt
@@ -12,20 +12,34 @@
 
 package id.thork.app.pages.main.element
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import id.thork.app.R
+import timber.log.Timber
 
 
-class MapFragment :Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var locationPermissionGranted = false
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    private var lastKnownLocation: Location? = null
+    private val DEFAULT_ZOOM = 17
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,25 +47,134 @@ class MapFragment :Fragment(), OnMapReadyCallback {
     ): View {
         val view: View = inflater.inflate(R.layout.fragment_map, container, false)
 
-        val mapFragment: SupportMapFragment? = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        val mapFragment: SupportMapFragment? =
+            childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(view.context);
 
+        getLocationPermission()
         // Inflate the layout for this fragment
+
         return view
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
         googleMap?.apply {
-            mMap = googleMap
-            val sydney = LatLng(-33.852, 151.211)
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(sydney)
-                    .title("Marker in Sydney")
+            map = googleMap
+            with(map) {
+                uiSettings.isMyLocationButtonEnabled = false
+                uiSettings.isMapToolbarEnabled = true
+            }
+        }
+        Timber.d("onMapReady() :%s", map.isMyLocationEnabled)
+
+//        this.map = googleMap!!
+//
+//        map.uiSettings.isMyLocationButtonEnabled = true
+//
+        getLocationPermission()
+        updateLocationUI()
+        getDeviceLocation()
+    }
+
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+            == PackageManager.PERMISSION_GRANTED) {
+            Timber.d("getLocationPermission() true")
+            locationPermissionGranted = true
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
             )
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         }
 
     }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+    }
+
+    private fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                map.isMyLocationEnabled = true
+                map.uiSettings.isMyLocationButtonEnabled = true
+                Timber.d("updateLocationUI(): %s", map.isMyLocationEnabled)
+                Timber.d("updateLocationUI(): button %s",  map.uiSettings.isMyLocationButtonEnabled )
+            } else {
+                map.isMyLocationEnabled = false
+                map.uiSettings.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+                Timber.d("updateLocationUI(): %s", map.isMyLocationEnabled)
+            }
+        } catch (e: SecurityException) {
+            Timber.d("updateLocationUI() catch: %s", e.toString())
+        }
+    }
+
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        lastKnownLocation = it.result
+                        if (lastKnownLocation != null) {
+                            map.isMyLocationEnabled = true
+                            map.uiSettings.isCompassEnabled
+                            map.uiSettings.isMyLocationButtonEnabled = true
+                            map.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(
+                                        lastKnownLocation!!.latitude,
+                                        lastKnownLocation!!.longitude
+                                    ), DEFAULT_ZOOM.toFloat()
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Timber.d("getDeviceLocation(): %s", e.message)
+        }
+    }
+
 }
