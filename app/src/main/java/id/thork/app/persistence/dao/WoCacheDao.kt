2b877455d100864c9aa9921e8af59d3e65da28d9
reja <reja@this.id>
2021-02-19 13:34:46 +0700

    [bugfix] - remove unused method

--- app/src/main/java/id/thork/app/persistence/dao/WoCacheDao.kt
+++ app/src/main/java/id/thork/app/persistence/dao/WoCacheDao.kt
@@ -32,7 +32,6 @@ interface WoCacheDao {
 
     fun findAllWo(offset: Int): List<WoCacheEntity>
 
-    fun findWoByWonum(offset: Int, wonum: String): List<WoCacheEntity>
     fun findWoByWonum(offset: Int, wonum: String, status: String): List<WoCacheEntity>
     fun findListWoByStatus(status: String): List<WoCacheEntity>
     fun findListWoByStatus(status: String, offset: Int): List<WoCacheEntity>
