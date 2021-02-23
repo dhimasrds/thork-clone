dd9556f035faa530da214a79beec0ff01458ba04
raka <raka@this.id>
2021-02-23 16:30:38 +0700

    hotfix-delete hardcode

--- app/src/main/java/id/thork/app/utils/LocaleHelper.kt
+++ app/src/main/java/id/thork/app/utils/LocaleHelper.kt
@@ -70,13 +70,4 @@ object LocaleHelper {
         }
         return context
     }
-//        val locale = Locale(language)
-//        Locale.setDefault(locale)
-//        val resources = context.resources
-//        val configuration = resources.configuration
-//        context.createConfigurationContext(configuration)
-//        configuration.setLocale(locale)
-//        resources.updateConfiguration(configuration, resources.displayMetrics)
-//        return true
-//    }
 }
