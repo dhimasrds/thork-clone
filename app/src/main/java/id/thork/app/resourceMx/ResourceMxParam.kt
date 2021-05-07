b81a76caf9987a5521210e8cf066e0fb5d2ed82d
this-dev <reza@this.id>
2021-05-07 12:51:00 +0700

    [feature] - add object AppResourceMx and ResourceMxParam

---
+++ app/src/main/java/id/thork/app/resourceMx/ResourceMxParam.kt
@@ -0,0 +1,10 @@
+package id.thork.app.resourceMx
+
+/**
+ * Created by M.Reza Sulaiman on 07/05/2021
+ * Jakarta, Indonesia.
+ */
+object ResourceMxParam {
+    const val FMS_RES_ASSET = "THISFSMRESasset"
+    const val FMS_RES_LOCATIONS = "THISFSMRESlocations"
+}
