3edc0b87c6890c511548683b8ab2b425c90792f0
reja <reja@this.id>
2021-05-18 13:42:14 +0700

    [feature] - add SSL Support on Glide

---
+++ app/src/main/java/id/thork/app/pages/example/ImageActivity.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2019 by This.ID, Indonesia . All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * This.ID. ("Confidential Information").
+ *
+ * Such Confidential Information shall not be disclosed and shall
+ * use it only	 in accordance with the terms of the license agreement
+ * entered into with This.ID; other than in accordance with the written
+ * permission of This.ID.
+ */
+
+package id.thork.app.pages.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.thork.app.R
+import id.thork.app.base.BaseActivity
+
+class ImageActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
