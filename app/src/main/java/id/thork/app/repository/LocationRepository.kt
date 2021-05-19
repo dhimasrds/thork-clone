29dcdb6a333d032335204d2f0471690547792d70
this-dev <reza@this.id>
2021-05-19 13:36:10 +0700

    [feature] - add module location repository

---
+++ app/src/main/java/id/thork/app/repository/LocationRepository.kt
@@ -0,0 +1,10 @@
+package id.thork.app.repository
+
+import id.thork.app.persistence.dao.LocationDaoImp
+
+/**
+ * Created by M.Reza Sulaiman on 19/05/2021
+ * Jakarta, Indonesia.
+ */
+class LocationRepository(locationDaoImp: LocationDaoImp) {
+}
