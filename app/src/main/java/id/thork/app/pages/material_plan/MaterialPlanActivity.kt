c85cca6603d760429baa6e7929fc7610768cde84
reja <reja@this.id>
2021-05-28 13:57:19 +0700

    [feature] - Material Plan Adapter

--- app/src/main/java/id/thork/app/pages/material_plan/MaterialPlanActivity.kt
+++ app/src/main/java/id/thork/app/pages/material_plan/MaterialPlanActivity.kt
@@ -15,16 +15,26 @@ package id.thork.app.pages.material_plan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.request.RequestOptions
 import dagger.hilt.android.AndroidEntryPoint
 import id.thork.app.R
 import id.thork.app.base.BaseActivity
 import id.thork.app.base.BaseParam
 import id.thork.app.databinding.ActivityAttachmentBinding
 import id.thork.app.databinding.ActivityMaterialPlanBinding
+import id.thork.app.di.module.PreferenceManager
 import id.thork.app.pages.attachment.AttachmentActivity
+import id.thork.app.pages.attachment.element.AttachmentAdapter
 import id.thork.app.pages.attachment.element.AttachmentViewModel
+import id.thork.app.pages.material_plan.element.MaterialPlanAdapter
 import id.thork.app.pages.material_plan.element.MaterialPlanViewModel
+import id.thork.app.persistence.entity.AttachmentEntity
+import id.thork.app.persistence.entity.MaterialEntity
 import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Named
 
 @AndroidEntryPoint
 class MaterialPlanActivity : BaseActivity() {
@@ -33,14 +43,38 @@ class MaterialPlanActivity : BaseActivity() {
     val viewModel: MaterialPlanViewModel by viewModels()
     private val binding: ActivityMaterialPlanBinding by binding(R.layout.activity_material_plan)
 
+    private lateinit var materialPlanAdapter: MaterialPlanAdapter
+    private lateinit var materialEntities: MutableList<MaterialEntity>
+
     private var intentWoId = 0
+
+    @Inject
+    @Named("svgRequestOption")
+    lateinit var svgRequestOptions: RequestOptions
+
+    @Inject
+    lateinit var preferenceManager: PreferenceManager
 
     override fun setupView() {
         super.setupView()
 
+        materialPlanAdapter =
+            MaterialPlanAdapter(this, preferenceManager, svgRequestOptions, materialEntities)
+
         binding.apply {
             lifecycleOwner = this@MaterialPlanActivity
             vm = viewModel
+
+            rvMaterials.apply {
+                layoutManager = LinearLayoutManager(this@MaterialPlanActivity)
+                addItemDecoration(
+                    DividerItemDecoration(
+                        this@MaterialPlanActivity,
+                        LinearLayoutManager.VERTICAL
+                    )
+                )
+                adapter = materialPlanAdapter
+            }
         }
 
         setupToolbarWithHomeNavigation(
