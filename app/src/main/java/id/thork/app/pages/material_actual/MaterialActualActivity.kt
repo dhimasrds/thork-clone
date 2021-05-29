a55111f2888c82d21c2d8a901f9cc79c86aaef8e
dhimasrds <dhimas@this.id>
2021-05-29 15:38:47 +0700

    [feature]-material Actual

--- app/src/main/java/id/thork/app/pages/material_actual/MaterialActualActivity.kt
+++ app/src/main/java/id/thork/app/pages/material_actual/MaterialActualActivity.kt
@@ -12,28 +12,65 @@
 
 package id.thork.app.pages.material_actual
 
+import android.content.Intent
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.request.RequestOptions
+import dagger.hilt.android.AndroidEntryPoint
 import id.thork.app.R
 import id.thork.app.base.BaseActivity
 import id.thork.app.base.BaseParam
 import id.thork.app.databinding.ActivityMaterialActualBinding
+import id.thork.app.di.module.PreferenceManager
+import id.thork.app.pages.material_actual.element.MaterialActualAdapter
 import id.thork.app.pages.material_actual.element.MaterialActualViewModel
+import id.thork.app.pages.material_plan.element.material_plan_list.MaterialPlanItem
+import id.thork.app.persistence.entity.WpmaterialEntity
 import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Named
 
+@AndroidEntryPoint
 class MaterialActualActivity  : BaseActivity() {
     val TAG = MaterialActualActivity::class.java.name
 
     val viewModel: MaterialActualViewModel by viewModels()
     private val binding: ActivityMaterialActualBinding by binding(R.layout.activity_material_actual)
 
+    private lateinit var materialActualAdapter: MaterialActualAdapter
+    private lateinit var wpmaterialList: MutableList<WpmaterialEntity>
+
     private var intentWoId = 0
+
+    @Inject
+    @Named("svgRequestOption")
+    lateinit var svgRequestOptions: RequestOptions
+
+    @Inject
+    lateinit var preferenceManager: PreferenceManager
 
     override fun setupView() {
         super.setupView()
+        wpmaterialList = mutableListOf()
+        materialActualAdapter =
+            MaterialActualAdapter(this, preferenceManager, svgRequestOptions, wpmaterialList)
 
         binding.apply {
             lifecycleOwner = this@MaterialActualActivity
             vm = viewModel
+
+            rvMaterials.apply {
+                layoutManager = LinearLayoutManager(this@MaterialActualActivity)
+                addItemDecoration(
+                    DividerItemDecoration(
+                        this@MaterialActualActivity,
+                        LinearLayoutManager.VERTICAL
+                    )
+                )
+                adapter = materialActualAdapter
+            }
         }
 
         setupToolbarWithHomeNavigation(
@@ -48,9 +85,26 @@ class MaterialActualActivity  : BaseActivity() {
         retrieveFromIntent()
     }
 
+    override fun setupListener() {
+        super.setupListener()
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(this, MaterialPlanItem::class.java)
+            startActivity(intent)
+        }
+    }
+
     private fun retrieveFromIntent() {
         intentWoId = intent.getIntExtra(BaseParam.WORKORDERID, 0)
         Timber.d("retrieveFromIntent() intentWoId: %s", intentWoId)
+        viewModel.initListMaterialPlan(intentWoId.toString())
     }
 
+    override fun setupObserver() {
+        super.setupObserver()
+        viewModel.listMaterial.observe(this, Observer {
+            wpmaterialList.clear()
+            wpmaterialList.addAll(it)
+            materialActualAdapter.notifyDataSetChanged()
+        })
+    }
 }
--- app/src/main/java/id/thork/app/pages/material_actual/element/MaterialActualViewModel.kt
+++ app/src/main/java/id/thork/app/pages/material_actual/element/MaterialActualViewModel.kt
@@ -14,11 +14,30 @@ package id.thork.app.pages.material_actual.element
 
 import android.content.Context
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.skydoves.whatif.whatIfNotNull
 import id.thork.app.base.LiveCoroutinesViewModel
+import id.thork.app.pages.material_plan.element.MaterialPlanViewModel
+import id.thork.app.persistence.entity.WpmaterialEntity
+import id.thork.app.repository.MaterialRepository
 
 class MaterialActualViewModel@ViewModelInject constructor(
-    private val context: Context
+    private val context: Context,
+    private val materialRepository: MaterialRepository
 ) : LiveCoroutinesViewModel() {
-    val TAG = MaterialActualViewModel::class.java.name
+    val TAG = MaterialPlanViewModel::class.java.name
+
+    private val _listMaterial = MutableLiveData<List<WpmaterialEntity>>()
+
+    val listMaterial: LiveData<List<WpmaterialEntity>> get() = _listMaterial
+
+
+    fun initListMaterialPlan(workorderid: String) {
+        val materialPlan = materialRepository.getListMaterialPlanByWoid(workorderid)
+        materialPlan.whatIfNotNull {
+            _listMaterial.value = it
+        }
+    }
 
 }
