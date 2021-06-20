a617a083c479aca6b709f70c62038990b2c29ee7
reja <reja@this.id>
2021-06-20 12:29:58 +0700

    [feature] - create LocomotifBuilder

--- app/src/main/java/id/thork/app/helper/builder/LocomotifValidator.kt
+++ app/src/main/java/id/thork/app/helper/builder/LocomotifValidator.kt
@@ -4,7 +4,7 @@ class LocomotifValidator<T> constructor(val item: T) {
     private val TAG = LocomotifValidator::class.java.name
 
     fun <T : Any> T.getListOfValues(variableName: String): Boolean {
-        return javaClass.getDeclaredField(variableName).let { field ->
+        javaClass.getDeclaredField(variableName).let { field ->
             field.isAccessible = true
             for (annotation in field.annotations) {
                 if (field.isAnnotationPresent(Lov::class.java)) {
