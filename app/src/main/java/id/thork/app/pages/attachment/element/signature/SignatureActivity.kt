8e897bdd0980a42ce2f1de183ef5ee3cb5cc8325
reja <reja@this.id>
2021-06-23 23:52:49 +0700

    [hotfix] - fix compability for android API below 29 (Q) signature preview

--- app/src/main/java/id/thork/app/pages/attachment/element/signature/SignatureActivity.kt
+++ app/src/main/java/id/thork/app/pages/attachment/element/signature/SignatureActivity.kt
@@ -17,6 +17,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.provider.MediaStore.Images
@@ -27,6 +28,8 @@ import id.thork.app.base.BaseActivity
 import id.thork.app.databinding.ActivitySignatureBinding
 import timber.log.Timber
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 
 
@@ -76,8 +79,13 @@ class SignatureActivity : BaseActivity() {
         binding.btnSave.setOnClickListener {
             Timber.tag(TAG).d("btnSaveListener()")
             binding.signaturePad.whatIfNotNull {
-                val uri = saveSignatureAsBitmap(this, it.signatureBitmap, Bitmap.CompressFormat.JPEG,
-                    "image/jpg", "signatured-".plus(System.currentTimeMillis().toString().plus(".jpg")) )
+                val uri = saveSignatureAsBitmap(
+                    this,
+                    it.signatureBitmap,
+                    Bitmap.CompressFormat.JPEG,
+                    "image/jpg",
+                    "signatured-".plus(System.currentTimeMillis().toString().plus(".jpg"))
+                )
                 uri.whatIfNotNull {
                     Timber.tag(TAG).d("btnSaveListener() uri: %s path: %s", it, it.path)
                     val intent = Intent()
@@ -89,40 +97,65 @@ class SignatureActivity : BaseActivity() {
         }
     }
 
-    @Throws(IOException::class)
-    fun saveSignatureAsBitmap(
-        context: Context, bitmap: Bitmap, format: Bitmap.CompressFormat,
-        mimeType: String, displayName: String
-    ): Uri {
+    @Suppress("DEPRECATION")
+    fun saveSignatureForOldApi(context: Context, bitmap: Bitmap, format: Bitmap.CompressFormat,
+                               displayName: String): Uri? {
+        Timber.tag(TAG).d("saveSignatureForOldApi() bitmap: %s", bitmap)
+        try {
+            val imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()
+            val imageFile = File(imageDir, displayName.plus(".jpg"))
+            val fos = FileOutputStream(imageFile)
+            bitmap.compress(format, 80, fos);
+            val uri = Uri.fromFile(imageFile)
+            return uri
+        } catch (e: Exception) {
+            Timber.tag(TAG).e("saveSignatureForOldApi() error: %s", e)
+        }
+        return null
+    }
 
+    fun saveSignature(context: Context, bitmap: Bitmap, format: Bitmap.CompressFormat,
+        mimeType: String, displayName: String): Uri? {
+        Timber.tag(TAG).d("saveSignature() bitmap: %s", bitmap)
         val values = ContentValues().apply {
             put(Images.Media.DISPLAY_NAME, displayName)
             put(Images.Media.MIME_TYPE, mimeType)
             put(Images.Media.DATE_TAKEN, System.currentTimeMillis())
-            put(Images.Media.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+            put(Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
         }
 
         val resolver = context.contentResolver
         var uri: Uri? = null
-
         try {
-            uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-                ?: throw IOException("Failed to create new MediaStore record.")
-
-            resolver.openOutputStream(uri)?.use {
-                if (!bitmap.compress(format, 95, it))
-                    throw IOException("Failed to save bitmap.")
-            } ?: throw IOException("Failed to open output stream.")
-            return uri
-
+            uri = resolver.insert(Images.Media.EXTERNAL_CONTENT_URI, values)
+            uri.whatIfNotNull {
+                resolver.openOutputStream(it)?.use {
+                    if (!bitmap.compress(format, 80, it))
+                        throw IOException("Failed to save bitmap.")
+                } ?: throw IOException("Failed to open output stream.")
+                return it
+            }
         } catch (e: IOException) {
-
             uri?.let { orphanUri ->
                 // Don't leave an orphan entry in the MediaStore
                 resolver.delete(orphanUri, null, null)
             }
-
+            Timber.tag(TAG).e("saveSignatureAsBitmap() error: %s", e)
             throw e
         }
+        return null
+    }
+
+    @Throws(IOException::class)
+    fun saveSignatureAsBitmap(
+        context: Context, bitmap: Bitmap, format: Bitmap.CompressFormat,
+        mimeType: String, displayName: String
+    ): Uri? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return saveSignature(context, bitmap, format, mimeType, displayName)
+        } else {
+            return saveSignatureForOldApi(context, bitmap, format, displayName)
+        }
+        return null
     }
 }
